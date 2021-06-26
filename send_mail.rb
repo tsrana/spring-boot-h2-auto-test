082327801769503009e0ccb5db7290fe752fdaba
Tejendra Rana <tejendra.rana@in.ibm.com>
2021-06-26 22:24:06 +0530

    6

--- send_mail.rb
+++ send_mail.rb
@@ -3,8 +3,8 @@
 require 'sendgrid-ruby'
 include SendGrid
 
-from = Email.new(email: 'test@example.com')
-to = Email.new(email: 'test@example.com')
+from = Email.new(email: 'thecloudteacher@gmail.com')
+to = Email.new(email: 'thecloudteacher@gmail.com')
 subject = 'Sending with SendGrid is Fun'
 content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
 mail = Mail.new(from, subject, to, content)
