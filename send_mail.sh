0dfdb6d9c267a98dbafb6964e3ff5e3635dd80f2
Tejendra Rana <tejendra.rana@in.ibm.com>
2021-06-27 00:46:27 +0530

    3

--- send_mail.sh
+++ send_mail.sh
@@ -5,7 +5,8 @@ cnt=`base64 emailable-report.html`
 echo "cnt -- $cnt"
 
 data='{"personalizations": [{"to": [{"email": "thecloudteacher@gmail.com"}]}],"from": {"email": "tsrana@gmail.com"},"subject":"Hello, World!","content": [{"type": "text/html","value": "Hey,<br>Please find attachment."}], "attachments": [{"content": "'$cnt'", "type": "text/html", "filename": "emailable-report.html"}]}'
+data1="'"$data"'"
 
-echo "data- $data"
+echo "data- $data1"
 
-curl --request POST --url https://api.sendgrid.com/v3/mail/send --header "authorization: Bearer $SENDGRID_API_KEY" --header 'Content-Type: application/json' --data "$data"
+curl --request POST --url https://api.sendgrid.com/v3/mail/send --header "authorization: Bearer $SENDGRID_API_KEY" --header 'Content-Type: application/json' --data "$data1"
