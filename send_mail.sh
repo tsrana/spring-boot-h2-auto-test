3d5a3fdc7801d5b27248e87a937f8ba67a29e3ed
Tejendra Rana <tejendra.rana@in.ibm.com>
2021-06-26 23:36:17 +0530

    2

--- send_mail.sh
+++ send_mail.sh
@@ -2,4 +2,4 @@ echo "key -- $SENDGRID_API_KEY"
 
 openssl base64 -in emailable-report.html -out emailable-report1.html
 
-curl --request POST --url https://api.sendgrid.com/v3/mail/send --header "authorization: Bearer $SENDGRID_API_KEY" --header 'Content-Type: application/json' --data '{"personalizations": [{"to": [{"email": "thecloudteacher@gmail.com"}]}],"from": {"email": "tsrana@gmail.com"},"subject":"Hello, World!","content": [{"type": "text/html","value": "Hey,<br>Please find attachment."}], "attachments": [{"content": "BASE64_ENCODED_CONTENT", "type": "text/html", "filename": "emailable-report1.html"}]}'
+curl --request POST --url https://api.sendgrid.com/v3/mail/send --header "authorization: Bearer $SENDGRID_API_KEY" --header 'Content-Type: application/json' --data '{"personalizations": [{"to": [{"email": "thecloudteacher@gmail.com"}]}],"from": {"email": "tsrana@gmail.com"},"subject":"Hello, World!","content": [{"type": "text/html","value": "Hey,<br>Please find attachment."}], "attachments": [{"content": "base64", "type": "text/html", "filename": "emailable-report1.html"}]}'
