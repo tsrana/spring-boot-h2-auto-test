d1eb1061770b6aa2fad8e0ce9bc648ac1cc95eca
Tejendra Rana <tejendra.rana@in.ibm.com>
2021-06-27 01:07:56 +0530

    3

--- send_mail.js
+++ send_mail.js
@@ -1,27 +1,15 @@
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
 javascript
 const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-
-const fs = require("fs")
-
-pathToAttachment = "/home/travis/build/tsrana/spring-boot-h2-auto-test/emailable-report.html"
-attachment = fs.readFileSync(pathToAttachment).toString("base64")
-
 const msg = {
-  to: 'thecloudteacher@gmail.com',
-  from: 'tsrana@gmail.com',
+  to: 'thecloudteacher@gmail.com', // Change to your recipient
+  from: 'tsrana@gmail.com', // Change to your verified sender
   subject: 'Sending with SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
-  attachments: [
-    {
-      content: attachment,
-      filename: "emailable-report.html",
-      type: "text/html",
-      disposition: "attachment"
-    }
-  ]
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
 }
-
 sgMail
   .send(msg)
   .then(() => {
